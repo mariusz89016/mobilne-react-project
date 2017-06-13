6f563b3412777b991d01d26577ed5cc3966fa70f
Michał Pałka <mpalka@virtuslab.com>
2017-06-13 23:55:43 +0200

    Basic game logic

--- src/main/scala/sri/mobile/template/components/ServerGameView.scala
+++ src/main/scala/sri/mobile/template/components/ServerGameView.scala
@@ -6,7 +6,7 @@ import sri.core.ReactComponent
 import sri.mobile.template.components.ClientGameView.State
 import sri.mobile.template.games
 import sri.mobile.template.games.PlayingCard.Joker
-import sri.mobile.template.games.mau.SimpleMauGame
+import sri.mobile.template.games.mau.{FinishedSimpleMauGame, SimpleMauGame, SimpleMauGameInProgress}
 import sri.mobile.template.games.{CardRank, CardSuit, PlayingCard, RankedCard}
 import sri.mobile.template.images.LikeImage
 import sri.mobile.template.messaging.encoding.TextDecoder
@@ -22,7 +22,7 @@ import scala.scalajs.js.typedarray.Uint8Array
 object ServerGameView {
   @ScalaJSDefined
   class Component() extends ReactComponent[Props, State] {
-    initialState(State(SimpleMauGame.newRandomGame()))
+    initialState(State(SimpleMauGame.newRandomGame(2)))
 
     val socket: Socket = Udp.createSocket("udp4")
     socket.bind(12345)
@@ -38,27 +38,32 @@ object ServerGameView {
     }
 
     override def componentDidMount(): Unit = {
-      val jsx1 = generateJsx(state.game.player1Hand).getBytes.toBase64
-      val msg1 =
-        s"""
-           |{
-           |  "command": "jsx",
-           |  "message": "${jsx1}"
-           |}
+      state.game match {
+        case SimpleMauGameInProgress(_, _, playerHands, _) =>
+          val jsx1 = generateJsx(playerHands(0)).getBytes.toBase64
+          val msg1 =
+            s"""
+               |{
+               |  "command": "jsx",
+               |  "message": "${jsx1}"
+               |}
          """.stripMargin.getBytes.toBase64
 
-      socket.send(msg1, 0, msg1.length, 12345, props.player1.ip)
+          socket.send(msg1, 0, msg1.length, 12345, props.player1.ip)
 
-      val jsx2 = generateJsx(state.game.player2Hand).getBytes.toBase64
-      val msg2 =
-        s"""
-           |{
-           |  "command": "jsx",
-           |  "message": "${jsx2}"
-           |}
+          val jsx2 = generateJsx(playerHands(1)).getBytes.toBase64
+          val msg2 =
+            s"""
+               |{
+               |  "command": "jsx",
+               |  "message": "${jsx2}"
+               |}
          """.stripMargin.getBytes.toBase64
 
-      socket.send(msg2, 0, msg2.length, 12345, props.player2.ip)
+          socket.send(msg2, 0, msg2.length, 12345, props.player2.ip)
+
+        case _ => {}
+      }
 
 
     }
@@ -73,18 +78,20 @@ object ServerGameView {
     }
 
     def render() = {
-      View()(
-        Text()("Server game view"),
-        JsxViewer(jsx = getServerStateJsx(), games.ComponentsMapper)
-//        CardView(RankedCard(CardRank.King, CardSuit.Hearts), uncovered = true)
-      )
+      state.game match {
+        case SimpleMauGameInProgress(_, uncoveredStack, _, _) =>
+          View()(
+            Text()("Game in progress"),
+            JsxViewer(jsx = generateJsx(uncoveredStack.head :: Nil), games.ComponentsMapper)
+          )
+
+        case FinishedSimpleMauGame(winnerNo) =>
+          View()(
+            Text()(s"The winner is player number ${winnerNo + 1}!")
+          )
+      }
+
     }
-
-    def getServerStateJsx() = {
-      generateJsx(state.game.uncoveredStack.head :: Nil)
-    }
-
-
   }
 
 
--- src/main/scala/sri/mobile/template/games/mau/SimpleMauGame.scala
+++ src/main/scala/sri/mobile/template/games/mau/SimpleMauGame.scala
@@ -1,11 +1,49 @@
 package sri.mobile.template.games.mau
 
+import sri.mobile.template.games.PlayingCard.Joker
 import sri.mobile.template.games.{CardRank, CardSuit, PlayingCard, RankedCard}
 
-case class SimpleMauGame(coveredStack: Seq[PlayingCard], uncoveredStack: Seq[PlayingCard], player1Hand: Seq[PlayingCard], player2Hand: Seq[PlayingCard]) {
+sealed trait SimpleMauGame
 
+case class SimpleMauGameInProgress(
+                          coveredStack: Seq[PlayingCard],
+                          uncoveredStack: Seq[PlayingCard],
+                          playerHands: Seq[Seq[PlayingCard]],
+                          currentPlayerNo: Int
+                        ) extends SimpleMauGame{
+  private def cardsMatch(lower: PlayingCard, upper: PlayingCard) = {
+    (lower, upper) match {
+      case (Joker, _) | (_, Joker) => true
+      case (RankedCard(_, suit1), RankedCard(_, suit2)) if suit1 == suit2 => true
+      case _ => false
+    }
+  }
 
+  def acceptMove(move: PlayerMove, playerNo: Int): Option[SimpleMauGame] = {
+    move match {
+      case PullCard =>
+        Some(this.copy(
+          coveredStack = coveredStack.tail,
+          currentPlayerNo = (currentPlayerNo + 1) % playerHands.length,
+          playerHands = playerHands.updated(currentPlayerNo, playerHands(currentPlayerNo) :+ coveredStack.head)
+        ))
+
+      case PushCard(card) =>
+        val topCard = uncoveredStack.head
+        if(playerNo == currentPlayerNo && cardsMatch(topCard, card)) {
+          Some(this.copy(
+            uncoveredStack = card +: uncoveredStack,
+            currentPlayerNo = (currentPlayerNo + 1) % playerHands.length,
+            playerHands = playerHands.updated(currentPlayerNo, playerHands(currentPlayerNo).diff(Seq(card)))
+          ))
+        } else {
+          None
+        }
+    }
+  }
 }
+
+case class FinishedSimpleMauGame(winnerNo: Int) extends SimpleMauGame
 
 object SimpleMauGame {
   val fullDeck = {
@@ -17,14 +55,23 @@ object SimpleMauGame {
     Seq.fill(2)(PlayingCard.Joker) ++ suitedCards
   }
 
-  def newRandomGame() = {
+  def newRandomGame(playersCount: Int): SimpleMauGameInProgress = {
     val shuffledDeck = scala.util.Random.shuffle(fullDeck).iterator
 
-    val player1Hand = shuffledDeck.take(5).toSeq
-    val player2Hand = shuffledDeck.take(5).toSeq
+    val playerHands = Seq.fill(playersCount)(shuffledDeck.take(5).toSeq)
+
     val uncoveredStack = shuffledDeck.take(1).toSeq
     val coveredStack = shuffledDeck.toSeq
 
-    SimpleMauGame(coveredStack, uncoveredStack, player1Hand, player2Hand)
+    if(uncoveredStack.head != Joker) {
+      SimpleMauGameInProgress(coveredStack, uncoveredStack, playerHands, currentPlayerNo = 0)
+    } else {
+      newRandomGame(playersCount)
+    }
   }
 }
+
+sealed trait PlayerMove
+
+case class PushCard(card: PlayingCard) extends PlayerMove
+object PullCard extends PlayerMove
